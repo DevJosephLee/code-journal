430d0be1ec0233c1e4babb25b64ccc75fb2523dc
Joseph Lee <teddylou0215@gmail.com>
2021-10-26 16:58:10 -0700

    Show the entry form if an edit icon was clicked.#4

--- js/main.js
+++ js/main.js
@@ -117,5 +117,5 @@ $entriesButton.addEventListener('click', entriesButtonClick);
 $newButton.addEventListener('click', newButtonClick);
 
 $entryList.addEventListener('click', function () {
-
+  switchViews('entry-form');
 });
