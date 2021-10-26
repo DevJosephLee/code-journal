5c314e4be484c65f0ddc25bb915c9ee3bfe14b0e
Joseph Lee <teddylou0215@gmail.com>
2021-10-25 17:45:05 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.#5

--- js/main.js
+++ js/main.js
@@ -24,3 +24,47 @@ $form.addEventListener('submit', function clickSubmit(event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  var $root = document.createElement('li');
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $root.appendChild($row);
+
+  var $columnHalfImg = document.createElement('div');
+  $columnHalfImg.setAttribute('class', 'column-half');
+  $row.appendChild($columnHalfImg);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', data.entries.photoUrl);
+  $columnHalfImg.appendChild($img);
+
+  var $columnHalfText = document.createElement('div');
+  $columnHalfText.setAttribute('class', 'column-half');
+  $row.appendChild($columnHalfText);
+
+  var $h3 = document.createElement('h3');
+  $h3.textContent = data.entries.title;
+  $columnHalfText.appendChild($h3);
+
+  var $p = document.createElement('p');
+  $p.textContent = data.entries.notes;
+  $columnHalfText.appendChild($p);
+
+  return $root;
+}
+
+renderEntry();
+
+//  < div class="row" >
+//               <div class="column-half">
+//                 <img src="images/placeholder-image-square.jpg" alt="placeholder">
+//               </div>
+//               <div class="column-half">
+//                 <h3>Ada Lovelace</h3>
+//                 <p>
+//                   blah blah blah
+//                 </p>
+//               </div>
+//             </div >
